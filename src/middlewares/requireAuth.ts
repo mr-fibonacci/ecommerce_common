0722c3fa1f55f1b16d533521a2e46bb7ba0af308
ad <adamlapinski1@gmail.com>
2020-08-01 18:31:57 +0100

    added currentUser prop to Request type in requireAuth middleware

--- src/middlewares/requireAuth.ts
+++ src/middlewares/requireAuth.ts
@@ -2,13 +2,30 @@ import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 import { NotAuthorizedError } from '../errors/notAuthorizedError';
 
+interface UserPayload {
+  id: string;
+  email: string;
+}
+
+declare global {
+  namespace Express {
+    interface Request {
+      currentUser?: UserPayload;
+    }
+  }
+}
+
 export const requireAuth = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
   try {
-    jwt.verify(req.session?.jwt, process.env.JWT_KEY!);
+    const { id, email } = jwt.verify(
+      req.session?.jwt,
+      process.env.JWT_KEY!
+    ) as UserPayload;
+    req.currentUser = { id, email };
   } catch (err) {
     throw new NotAuthorizedError();
   }
